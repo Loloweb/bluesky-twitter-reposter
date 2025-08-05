3b20df7fb1eb33b14a58771396415d2d2b5a0ba1
Lolo <lowgau@gmail.com>
2025-08-05 16:57:11 +0200

    remove unused constants

--- client.py
+++ client.py
@@ -1,9 +1,4 @@
-# Twitter account (needed for scraping, not the account being scraped)
-USERNAME = ''
-EMAIL = ''
-PASSWORD = ''
 # Twitter account to scrape
-SCRAPE_ID = ''
 SCRAPE_USERNAME = ''
 # Bluesky account (needed for posting)
 BLUESKY_USERNAME=""
